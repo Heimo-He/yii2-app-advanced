1bed56af81daf5bee74b87cd21704af3ca3565fa
heqinhui_93@163.com <heimo.he@tophant.com>
2018-06-05 15:23:14 +0800

    移除第三方rbac，修改migration初始化脚本

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -15,19 +15,32 @@ class m130524_201442_init extends Migration
         $this->createTable('{{%user}}', [
             'id' => $this->primaryKey(),
             'username' => $this->string()->notNull()->unique(),
+            'access_token' => $this->string()->notNull()->unique(),
             'auth_key' => $this->string(32)->notNull(),
             'password_hash' => $this->string()->notNull(),
             'password_reset_token' => $this->string()->unique(),
             'email' => $this->string()->notNull()->unique(),
-
             'status' => $this->smallInteger()->notNull()->defaultValue(10),
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
         ], $tableOptions);
+
+        $this->initSql();
     }
 
     public function down()
     {
         $this->dropTable('{{%user}}');
+    }
+
+    public function initSql(){
+        $now = time();
+        $initSql = <<<SQL
+INSERT INTO `mcs_user` (`username`, `access_token`, `auth_key`, `password_hash`, `password_reset_token`, `email`, `status`, `created_at`, `updated_at`)
+VALUES
+	('root', '', '', '$2y$10\$yLdDeWlbHWgQht2S.8m7VeLssUquehUrAJRwCA4wjFIpkGCm7yqyC', '', '', 10, $now, $now);
+SQL;
+
+        Yii::$app->db->createCommand($initSql)->execute();
     }
 }
